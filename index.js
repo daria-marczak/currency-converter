4953e1c1aa22c5005678baaa7a28e307c4c15e13
Daria Marczak <marczak.ria@gmail.com>
2018-02-04 17:20:34 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,45 @@
+var inputAmount = document.getElementById("amount");
+var inputCurrency = document.getElementById("inputCurrency");
+var outputAmount = document.getElementById("output");
+var outputCurrency = document.getElementById("outputCurrency");
+
+var button = document.getElementById("calculate");
+
+var givenAmount = document.getElementById("givenAmount");
+var baseCurrency = document.getElementById("baseCurrency");
+var finalAmount = document.getElementById("finalAmount");
+var finalCurrency = document.getElementById("finalCurrency");
+
+var xHttp = new XMLHttpRequest();
+
+function loadCurrencies() {
+	xHttp.onreadystatechange = function() {
+		if(xHttp.readyState == 4 && xHttp.status == 200) {
+			var obj = JSON.parse(this.responseText);
+			var options = "";
+			for (key in obj.rates) {
+				options = options + "<option>" + key + "</option>";
+			}
+			inputCurrency.innerHTML = options;
+			outputCurrency.innerHTML = options;
+		}
+		xHttp.open("GET", "https://api.fixer.io/latest", true);
+		xHttp.send();
+	}
+}
+
+function convertCurrency() {
+	if (baseCurrency.length > 0 && finalCurrency.length > 0 && amount > 0) {
+		xHttp.onreadystatechange = function() {
+			if(xHttp.readyState == 4 && xHttp.status == 200) {
+				var obj = JSON.parse(this.responseText);
+				var fact = parseFloat(obj.rates[inputCurrency]);
+				if (fact != undefined) {
+					result.innerHTML = parseFloat(amount)*fact;
+				}
+			}
+		}
+		xHttp.open("GET", "https://api.fixer.io/latest?base=" + inputCurrency.value + "&symbols=" + outputCurrency.value, true);
+		
+	}
+}
